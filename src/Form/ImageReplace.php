f4ecce44f32f354a8bce866c90546fab167dcbe3
Andrew Farquharson <farquharson.a@gmail.com>
2019-01-17 13:04:14 +0000

    form render array

--- src/Form/ImageReplace.php
+++ src/Form/ImageReplace.php
@@ -29,7 +29,7 @@ class ImageReplace extends FormBase
             '#type' => 'radios',
             '#title' => $this->t('Image radio buttons'),
             '#description' => $this->t('none'),
-            '#options' => ['Ruby' => $this->t('Ruby'), 'Topaz' => $this->t('Topaz'), 'Diamond' => $this->t('Diamond')],
+            '#options' => ['Image1' => $this->t('Image1'), 'Image2' => $this->t('Image2'), 'Image3' => $this->t('Image3'), 'Image4' => $this->t('Image4'), 'Image5' => $this->t('Image5'),],
             '#weight' => '0',
         ];
         $form['submit1'] = [
@@ -44,7 +44,7 @@ class ImageReplace extends FormBase
             '#type' => 'checkboxes',
             '#title' => $this->t('Image checkbox buttons'),
             '#description' => $this->t('none'),
-            '#options' => ['Ruby' => $this->t('Ruby'), 'Topaz' => $this->t('Topaz'), 'Diamond' => $this->t('Diamond')],
+            '#options' => ['Image1' => $this->t('Image1'), 'Image2' => $this->t('Image2'), 'Image3' => $this->t('Image3'), 'Image4' => $this->t('Image4'), 'Image5' => $this->t('Image5'),],
             '#weight' => '0',
         ];
         $form['submit2'] = [
